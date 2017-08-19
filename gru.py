abe95bd0033562c9524796e0c29d2c106f04880c
Akuzeee <tktktktkdkdkdkdk@gmail.com>
2017-08-19 17:13:53 +0900

    Add initHidden to gru

--- gru.py
+++ gru.py
@@ -4,7 +4,7 @@ from torch.autograd import Variable
 import torch.nn.init as init
 
 class GRU(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size, num_layers=1, dropout=0.2, weight_init=True, init_forget_bias=1):
+    def __init__(self, input_size, hidden_size, output_size, num_layers=1, dropout=0.2):
         super(GRU, self).__init__()
 
         self.hidden_size = hidden_size
@@ -13,19 +13,16 @@ class GRU(nn.Module):
         self.gru = nn.GRU(input_size, hidden_size, num_layers=num_layers, dropout=dropout)
         self.linear = nn.Linear(hidden_size, output_size)
         
-        self.init_forget_bias = init_forget_bias
-        if weight_init == True:
-            self.weight_init()
 
-    def forward(self, inputs, hidden):
-        _, self.ht = self.gru(inputs, hidden)
+    def forward(self, inputs):
+        _, self.hidden = self.gru(inputs, self.hidden)
         # extract the last hidden layer from ht(n_layers, n_samples, hidden_size)
-        htL = self.ht[-1]
+        htL = self.hidden[-1]
         outputs = self.linear(htL)
         return outputs
     
-    def weight_init(self):
-        # See https://github.com/pytorch/pytorch/blob/master/torch/nn/modules/rnn.py
+    def weight_init(self, init_forget_bias):
+        # See details in https://github.com/pytorch/pytorch/blob/master/torch/nn/modules/rnn.py
         for name, params in self.named_parameters(): 
             # weightをxavierで初期化
             if 'weight' in name:
@@ -34,15 +31,18 @@ class GRU(nn.Module):
             # 忘却しやすくなるようGRUのb_iz, b_hzを初期化
             elif 'gru.bias_ih_l' in name:
                 b_ir, b_iz, b_in = params.chunk(3, 0)
-                init.constant(b_iz, self.init_forget_bias)
+                init.constant(b_iz, init_forget_bias)
             elif 'gru.bias_hh_l' in name:
                 b_hr, b_hz, b_hn = params.chunk(3, 0)
-                init.constant(b_hz, self.init_forget_bias)
+                init.constant(b_hz, init_forget_bias)
 
             # それ以外のbiasを0に初期化
             else:
                 init.constant(params, 0)
-        
 
+    def init_hidden(self, batch_size, gpu=True):
+        if gpu == True:
+            self.hidden = Variable(torch.randn(self.num_layers, batch_size, self.hidden_size).cuda())
+        else:
+            self.hidden = Variable(torch.randn(self.num_layers, batch_size, self.hidden_size))
 
-           
