4662f7204038a1565cc373f035328d14c1250bee
Akuzeee <tktktktkdkdkdkdk@gmail.com>
2017-08-20 11:36:10 +0900

    Add sru model

--- sru.py
+++ sru.py
@@ -2,65 +2,82 @@ import torch
 import torch.nn as nn
 from torch.autograd import Variable
 import torch.nn.functional as F
+import torch.nn.init as init
 
 
 class SRU(nn.Module):
-    def __init__(self, x_dim, phi_dim, r_dim, o_dim, A, GPU=True):
-        """ 
-        x_dim:   入力xの次元（特徴量数）
-        phi_dim: phiの次元。\mu^{\alpha}の次元とも等しい
-        r_dim:   rの次元
-        o_dim:   出力oの次元
-        A:       [\alpha_1, \alpha_2, ..., \alpha_m], shape: (1, m)
+    def __init__(self, input_size, phi_size, r_size, output_size, A=[0, 0.5, 0.9, 0.99], gpu=True):
+        """
+        input_size:   入力xの特徴量数
+        phi_size:     phiのユニット数。\mu^{\alpha}の次元とも等しい
+        r_size:       rのユニット数。
+        output_size:  出力outputの次元
+        A:            [\alpha_1, \alpha_2, ..., \alpha_m]
         """
 
         super(SRU, self).__init__()
 
-        self.gpu     = GPU
-        n_alpha      = A.size()[1]
-        self.n_alpha = n_alpha
-        self.A       = A
-        self.phi_dim = phi_dim
-        # muの次元 = phiの次元*alphaの個数
-        mu_dim = phi_dim * n_alpha 
-        self.mu_dim = mu_dim
-        
-        # 各結合の定義
-        self.mu2r    = nn.Linear(mu_dim, r_dim)
-        self.xr2phi  = nn.Linear(x_dim + r_dim, phi_dim)
-        self.mu2o    = nn.Linear(mu_dim, o_dim)
-        self.log_softmax = nn.LogSoftmax()
+        self._gpu = gpu
+        self.n_alpha      = len(A)
+        # Aをリストからtensorに変換（後の計算のため）
+        if self._gpu == True:
+            self.A       = torch.Tensor(A).view(1, -1).cuda()
+        else:
+            self.A       = torch.Tensor(A).view(1, -1)
+        self.phi_size = phi_size
+        # muのユニット数 = phiのユニット数 * alphaの個数
+        self.mu_size = self.phi_size * self.n_alpha
 
-    def forward(self, x, mu):
+        # 各結合の定義
+        self.mu2r    = nn.Linear(self.mu_size, r_size)
+        self.xr2phi  = nn.Linear(input_size + r_size, phi_size)
+        self.mu2o    = nn.Linear(self.mu_size, output_size)
+
+    def forward(self, inputs):
         '''
-        x.size()  => (sample_size, x_dim)
+        inputs.size()  => (seq_len, sample_size, x_dim)
         mu.size() => (sample_size, mu_dim)
         '''
+        for x in inputs:
+            r = F.relu(self.mu2r(self.mu))
+            phi = F.relu(self.xr2phi(torch.cat((x, r), 1)))
+            self.mu = self.muphi2mu(self.mu, phi)
 
-        r = F.relu(self.mu2r(mu))
-        phi = F.relu(self.xr2phi(torch.cat((x, r), 1)))
-        mu = self.muphi2mu(mu, phi)
-        o = F.relu(self.mu2o(mu))
-        o = self.log_softmax(o)
-        return o, mu
-    
+        outputs = F.relu(self.mu2o(self.mu))
+        return outputs
+
     def muphi2mu(self, mu, phi):
         '''
         すべての\alphaについて、\mu_t^{(\alpha)} = \alpha \mu_{t-1}^{(\alpha)} + (1-\alpha) \phi_t を同時に行う
-            A_mask:   Kronecker product of (A, ones(1, phi_dim)),   shape => (1, mu_dim)
+            A_mask:   Kronecker product of (A, ones(1, phi_size)),   shape => (1, mu_dim)
             phi_tile: Kronecker product of (ones(1, n_alpha), phi), shape => (sample_size, mu_dim)
         '''
-        if self.gpu:
-            A_mask = kronecker_product(self.A, torch.ones(1, self.phi_dim).cuda())
+        if self._gpu:
+            A_mask = kronecker_product(self.A, torch.ones(1, self.phi_size).cuda())
             phi_tile = kronecker_product(Variable(torch.ones(1, self.n_alpha).cuda()), phi)
         else:
-            A_mask = kronecker_product(self.A, torch.ones(1, self.phi_dim))
+            A_mask = kronecker_product(self.A, torch.ones(1, self.phi_size))
             phi_tile = kronecker_product(Variable(torch.ones(1, self.n_alpha)), phi)
 
         # 要素積をとるためにA_maskをVariableに変換するが、A_maskは定数項なのでrequires_grad=Falseをつける
         A_mask = Variable(A_mask, requires_grad=False)
         mu = torch.mul(A_mask, mu) + torch.mul((1-A_mask), phi_tile)
         return mu
+
+    def initWeight(self):
+        for name, params in self.named_parameters():
+            # weightをxavierで初期化
+            if 'weight' in name:
+                init.xavier_uniform(params)
+            # それ以外のbiasを0に初期化
+            else:
+                init.constant(params, 0)
+
+    def initHidden(self, batch_size):
+        if self._gpu == True:
+            self.mu = Variable(torch.zeros(batch_size, self.mu_size)).cuda()
+        else:
+            self.mu = Variable(torch.zeros(batch_size, self.mu_size))
 
 
 def kronecker_product(t1, t2):
@@ -78,5 +95,6 @@ def kronecker_product(t1, t2):
     )
 
     return expanded_t1 * tiled_t2
+
 
 
