25348b6af5814072f743eae6528343617ef6f639
Akuzeee <tktktktkdkdkdkdk@gmail.com>
2017-08-23 10:59:10 +0900

    Refactor hyperopt

---
+++ main.py
@@ -0,0 +1,215 @@
+import argparse
+import time
+import math
+import numpy as np
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import fetch_mldata
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim.lr_scheduler import StepLR
+from torch.autograd import Variable
+from hyperopt import fmin, tpe, hp, rand
+from models import SRU, GRU, LSTM
+
+
+''' コマンドライン引数の設定 '''
+
+parser = argparse.ArgumentParser(description='Run hyperopt')
+parser.add_argument('model', type=str, nargs='?', default='sru',
+                     help='select your model from [sru, gru, lstm]')
+parser.add_argument('--each-epoch', type=int, nargs='?', default=10,
+                     help='select num of epochs for each trial')
+parser.add_argument('--seed', type=int, nargs='?', default=0,
+                     help='set random seed')
+parser.add_argument('--devise-id', type=int, nargs='?', default=0,
+                     help='select gpu devise id')
+args       = parser.parse_args()
+model_name = args.model
+n_epochs   = args.each_epoch
+seed       = args.seed
+devise_id  = args.devise_id
+
+torch.cuda.manual_seed(seed)
+torch.cuda.set_device(devise_id)
+print('Bayesian optimization of %s' % model_name))
+
+''' データセット準備 '''
+
+def load_mnist():
+    mnist = fetch_mldata('MNIST original')
+    mnist_X, mnist_y = shuffle(mnist.data, mnist.target, random_state=seed)
+    mnist_X = mnist_X / 255.0
+
+    # pytorch用に型変換
+    mnist_X, mnist_y = mnist_X.astype('float32'), mnist_y.astype('int64')
+
+    # 2次元の画像を、各行を互い違いにして1次元に変換
+    def flatten_img(images):
+        '''
+        images: shape => (n, rows, columns)
+        output: shape => (n, rows*columns)
+        '''
+        n_rows    = images.shape[1]
+        n_columns = images.shape[2]
+        for num in range(n_rows):
+            if num % 2 != 0:
+                images[:, num, :] = images[:, num, :][:, ::-1]
+        output = images.reshape(-1, n_rows*n_columns)
+        return output
+
+    mnist_X = mnist_X.reshape(-1, 28, 28)
+    mnist_X = flatten_img(mnist_X) # X.shape => (n_samples, seq_len)
+    mnist_X = mnist_X[:, :, np.newaxis] # X.shape => (n_samples, seq_len, n_features)
+
+    # 訓練、テストデータに分割
+    train_X, test_X, train_y, test_y = train_test_split(mnist_X, mnist_y,
+                                                        test_size=0.2,
+                                                        random_state=seed)
+    return train_X, test_X, train_y, test_y
+
+
+''' 訓練の準備 '''
+
+# 計算時間を表示させる
+def timeSince(since):
+    now = time.time()
+    s = now - since
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+# batchあたりの訓練
+def train(model, inputs, labels, optimizer, criterion, clip):
+    batch_size = inputs.size(1)
+    model.initHidden(batch_size) # 隠れ変数の初期化
+    optimizer.zero_grad() # 勾配の初期化
+    outputs = model(inputs)
+    loss = criterion(outputs, labels)
+    torch.nn.utils.clip_grad_norm(model.parameters(), clip) # gradient clipping
+    loss.backward()
+    optimizer.step()
+    acc = (torch.max(outputs, 1)[1] == labels).sum().data[0] / batch_size
+    return loss.data[0], acc
+
+
+# 検証
+def testate(model, inputs, labels, optimizer, criterion):
+    batch_size = inputs.size(1)
+    model.initHidden(batch_size)
+    outputs = model(inputs)
+    loss = criterion(outputs, labels)
+    acc = (torch.max(outputs, 1)[1] == labels).sum().data[0] / batch_size
+    return loss.data[0], acc
+
+
+''' パラメータの準備 '''
+
+if model_name == 'sru':
+    parameter_space = {
+        'phi_size': hp.quniform('phi_size', 1, 256, q=1),
+        'r_size':   hp.quniform('r_size', 1, 64, q=1),
+        'cell_out_size': hp.quniform('cell_out_size', 1, 256, q=1)
+    }
+elif model_name in ['gru', 'lstm']:
+    parameter_space = {
+        'hidden_size': hp.quniform('hidden_size', 1, 256, q=1),
+        'num_layers':  hp.quniform('num_layers', 1, 5, q=1),
+        'init_forget_bias': hp.uniform('init_forget_bias', 0, 20)
+    }
+parameter_space.update({
+        'l_rate': hp.loguniform('l_rate', -10, 0),
+        'lr_decay':hp.uniform('lr_decay', 0.8, 0.999),
+        'dropout':hp.uniform('dropout', 0, 1),
+        'clip': hp.loguniform('clip', 0, 10)
+    })
+
+
+''' 目的関数の定義 '''
+
+def objective(args):
+    print(args)
+    if model_name == 'sru':
+        phi_size      = int(args['phi_size'])
+        r_size        = int(args['r_size'])
+        cell_out_size = int(args['cell_out_size'])
+    elif model_name in ['gru', 'lstm']:
+        hidden_size = int(args['hidden_size'])
+        num_layers  = int(args['num_layers'])
+        init_forget_bias = args['init_forget_bias']
+    lr            = args['l_rate']
+    lr_decay      = args['lr_decay']
+    dropout       = args['dropout']
+    clip          = args['clip']
+
+    train_X, test_X, train_y, test_y = load_mnist()
+    input_size = train_X.shape[2]
+    output_size = np.unique(train_y).size
+
+    # モデルのインスタンスの作成
+    if model_name == 'sru':
+        model = SRU(input_size, phi_size, r_size, cell_out_size, output_size, dropout=dropout)
+        model.initWeight()
+    elif model_name == 'gru':
+        model = GRU(input_size, hidden_size, output_size, num_layers, dropout)
+        model.initWeight(init_forget_bias)
+    elif model_name == 'lstm':
+        model = LSTM(input_size, hidden_size, output_size, num_layers, dropout)
+        model.initWeight(init_forget_bias)
+    model.cuda()
+
+    # loss, optimizerの定義
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=lr)
+    scheduler = StepLR(optimizer, step_size=100, gamma=lr_decay)
+
+
+    ''' 訓練 '''
+    batch_size = 128
+    n_batches = train_X.shape[0]//batch_size
+    n_batches_test = test_X.shape[0]//batch_size
+    all_acc = []
+    start_time = time.time()
+
+    for epoch in range(n_epochs):
+        train_cost, test_cost, train_acc, test_acc  = 0, 0, 0, 0
+        train_X, train_y = shuffle(train_X, train_y, random_state=seed+epoch)
+
+        # 訓練
+        model.train()
+        train_X_t = np.transpose(train_X, (1, 0, 2)) # X.shape => (seq_len, n_samples, n_features) に変換
+        for i in range(n_batches):
+            scheduler.step()
+            start = i * batch_size
+            end = start + batch_size
+            inputs, labels = train_X_t[:, start:end, :], train_y[start:end]
+            inputs, labels = Variable(torch.from_numpy(inputs).cuda()
+                             ), Variable(torch.from_numpy(labels).cuda())
+            cost, accuracy = train(model, inputs, labels, optimizer, criterion, clip)
+            train_cost += cost / n_batches
+            train_acc  += accuracy / n_batches
+
+        # 検証
+        model.eval()
+        test_X_t = np.transpose(test_X, (1, 0, 2))
+        for i in range(n_batches_test):
+            start = i * batch_size
+            end = start + batch_size
+            inputs, labels = test_X_t[:, start:end, :], test_y[start:end]
+            inputs, labels = Variable(torch.from_numpy(inputs).cuda()
+                             ), Variable(torch.from_numpy(labels).cuda())
+            cost, accuracy = testate(model, inputs, labels, optimizer, criterion)
+            test_cost += cost / n_batches_test
+            test_acc += accuracy / n_batches_test
+
+        all_acc.append(test_acc)
+        print('EPOCH:: %i, (%s) train_cost: %.3f, test_cost: %.3f, train_acc: %.3f, test_acc: %.3f' % (epoch + 1,
+                           timeSince(start_time), train_cost, test_cost, train_acc, test_acc))
+
+    # test_accの最大値を返す
+    return max(all_acc)
+
+best = fmin(objective, parameter_space, algo=rand.suggest, max_evals=100)
+print(best)
+
---
+++ models.py
@@ -0,0 +1,177 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+import torch.nn.init as init
+
+
+class SRU(nn.Module):
+    def __init__(self, input_size, phi_size, r_size, cell_out_size, output_size, A=[0, 0.5, 0.9, 0.99, 0.999], dropout=0, gpu=True):
+        """
+        input_size:　  inputの特徴量数
+        phi_size:      phiのユニット数。\mu^{\alpha}の次元とも等しい
+        r_size:        rのユニット数
+        cell_out_size: SRUCellからの出力のunit数
+        output_size:   outputの次元
+        A:             [\alpha_1, \alpha_2, ..., \alpha_m]
+        """
+
+        super(SRU, self).__init__()
+
+        self._gpu = gpu
+        self.n_alpha  = len(A)
+        self.phi_size = phi_size
+        self.mu_size  = self.phi_size * self.n_alpha # muのユニット数 = phiのユニット数 * alphaの個数
+
+        # 各結合の定義
+        self.mu2r   = nn.Linear(self.mu_size, r_size)
+        self.xr2phi = nn.Linear(input_size + r_size, phi_size)
+        self.mu2o   = nn.Linear(self.mu_size, cell_out_size)
+        self.drop   = nn.Dropout(p=dropout)
+        self.linear = nn.Linear(cell_out_size, output_size) 
+        
+        # muphi2phiの準備
+        # A_mask: Kronecker product of (A, ones(1, phi_size)),  shape => (1, mu_dim)
+        if self._gpu == True:
+            self.A_mask = torch.Tensor([x for x in(A) for i in range(phi_size)]).view(1, -1).cuda()
+        else:
+            self.A_mask = torch.Tensor([x for x in(A) for i in range(phi_size)]).view(1, -1)
+        # A_maskは定数項なのでrequires_grad=Falseをつける
+        self.A_mask = Variable(self.A_mask, requires_grad=False)
+
+    def forward(self, inputs):
+        '''
+        inputs.size()  => (seq_len, sample_size, x_dim)
+        mu.size() => (sample_size, mu_dim)
+        '''
+        for x in inputs:
+            r = F.relu(self.mu2r(self.mu))
+            phi = F.relu(self.xr2phi(torch.cat((x, r), 1)))
+            self.mu = self.muphi2mu(self.mu, phi)
+        cell_out = F.relu(self.mu2o(self.mu))
+        cell_out = self.drop(cell_out)
+        outputs  = self.linear(cell_out)
+        return outputs
+
+    def muphi2mu(self, mu, phi):
+        '''
+        数式: \mu = A_mask * \mu + (1-A_mask) * phi_tile
+            A_mask:   Kronecker product of (A, ones(1, phi_size)),  shape => (1, mu_dim)
+            phi_tile: Kronecker product of (ones(1, n_alpha), phi), shape => (sample_size, mu_dim)
+        '''
+        phi_tile = phi.repeat(1, self.n_alpha)
+        mu = torch.mul(self.A_mask, mu) + torch.mul((1-self.A_mask), phi_tile)
+        return mu
+
+    def initWeight(self):
+        for name, params in self.named_parameters():
+            # weightをxavierで初期化
+            if 'weight' in name:
+                init.xavier_uniform(params, init.calculate_gain('relu'))
+            # biasを0で初期化
+            else:
+                init.constant(params, 0)
+
+    def initHidden(self, batch_size):
+        if self._gpu == True:
+            self.mu = Variable(torch.zeros(batch_size, self.mu_size)).cuda()
+        else:
+            self.mu = Variable(torch.zeros(batch_size, self.mu_size))
+
+
+class GRU(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, num_layers=1, dropout=0, gpu=True):
+        super(GRU, self).__init__()
+
+        self._gpu        = gpu
+        self.hidden_size = hidden_size
+        self.num_layers  = num_layers
+        
+        # 各layerの定義
+        self.gru = nn.GRU(input_size, hidden_size, num_layers=num_layers)
+        self.drop = nn.Dropout(p=dropout)
+        self.linear = nn.Linear(hidden_size, output_size)
+
+    def forward(self, inputs):
+        _, self.hidden = self.gru(inputs, self.hidden)
+        # extract the last hidden layer from ht(n_layers, n_samples, hidden_size)
+        htL = self.hidden[-1]
+        htL = self.drop(htL)
+        outputs = self.linear(htL)
+        return outputs
+    
+    def initWeight(self, init_forget_bias=1):
+        # See details in https://github.com/pytorch/pytorch/blob/master/torch/nn/modules/rnn.py
+        for name, params in self.named_parameters(): 
+            # weightをxavierで初期化
+            if 'weight' in name:
+                init.xavier_uniform(params)
+
+            # 忘却しやすくなるようGRUのb_iz, b_hzを初期化
+            elif 'gru.bias_ih_l' in name:
+                b_ir, b_iz, b_in = params.chunk(3, 0)
+                init.constant(b_iz, init_forget_bias)
+            elif 'gru.bias_hh_l' in name:
+                b_hr, b_hz, b_hn = params.chunk(3, 0)
+                init.constant(b_hz, init_forget_bias)
+
+            # それ以外のbiasを0に初期化
+            else:
+                init.constant(params, 0)
+
+    def initHidden(self, batch_size):
+        if self._gpu == True:
+            self.hidden = Variable(torch.zeros(self.num_layers, batch_size, self.hidden_size).cuda())
+        else:
+            self.hidden = Variable(torch.zeros(self.num_layers, batch_size, self.hidden_size))
+
+
+class LSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, num_layers=1, dropout=0, gpu=True):
+        super(LSTM, self).__init__()
+
+        self._gpu        = gpu
+        self.hidden_size = hidden_size
+        self.num_layers  = num_layers
+
+        # 各layerの定義
+        self.lstm   = nn.LSTM(input_size, hidden_size, num_layers)
+        self.drop   = nn.Dropout(p=dropout)
+        self.linear = nn.Linear(hidden_size, output_size)
+        
+
+    def forward(self, inputs):
+        # hidden = (h_t, c_t)
+        _, self.hidden = self.lstm(inputs, self.hidden)
+        # extract the last hidden layer from h_t(n_layers, n_samples, hidden_size)
+        htL = self.hidden[0][-1]
+        htL = self.drop(htL)
+        outputs = self.linear(htL)
+        return outputs
+    
+    def initWeight(self, init_forget_bias=1):
+        # See https://github.com/pytorch/pytorch/blob/master/torch/nn/modules/rnn.py
+        for name, params in self.named_parameters(): 
+            # weightをxavierで初期化
+            if 'weight' in name:
+                init.xavier_uniform(params)
+
+            # 忘却しやすくなるようLSTMのb_if, b_hfを初期化
+            elif 'lstm.bias_ih_l' in name:
+                b_ii, b_if, b_ig, b_i0 = params.chunk(4, 0)
+                init.constant(b_if, init_forget_bias)
+            elif 'lstm.bias_hh_l' in name:
+                b_hi, b_hf, b_hg, b_h0 = params.chunk(4, 0)
+                init.constant(b_hf, init_forget_bias)
+
+            # それ以外のbiasを0に初期化
+            else:
+                init.constant(params, 0)
+
+    def initHidden(self, batch_size, gpu=True):
+        if gpu == True:
+            self.hidden = (Variable(torch.zeros(self.num_layers, batch_size, self.hidden_size).cuda()),
+                           Variable(torch.zeros(self.num_layers, batch_size, self.hidden_size).cuda()))
+        else:
+            self.hidden = (Variable(torch.zeros(self.num_layers, batch_size, self.hidden_size)),
+                           Variable(torch.zeros(self.num_layers, batch_size, self.hidden_size)))
